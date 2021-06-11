883ba9ac4db61a5aae439cc47f477d65ca955b12
VVassoler <vvassolern@gmail.com>
2021-06-11 09:22:12 -0300

    commit final DIO

---
+++ README.md
@@ -0,0 +1,7 @@
+#### BOOKFLIX :books:
+
+Usando a similaridade da interface do site de filmes da NETFLIX foi criada a interface atribuindo livros da literatura clássica....
+
+* HTML
+* CSS
+* JAVASCRIPT
---
+++ js/action.js
@@ -0,0 +1,7 @@
+function redireciona(){
+    window.location.href = "http://www.dominiopublico.gov.br/pesquisa/DetalheObraForm.do?select_action=&co_obra=1888";
+}
+
+function informacao(){
+    alert("Título: Dom Casmurro\nAutor: Machado de Assis\nCategoria: Literatura\nIdioma: Português");
+}
